da304f53637394a8dcc9ad2c845b2f1a9be309c4
Luis Barrera <124116776+Lbar23@users.noreply.github.com>
2025-09-22 16:14:18 -0500

    updated ICourseRepository

--- backend/Interfaces/ICourseRepository.cs
+++ backend/Interfaces/ICourseRepository.cs
@@ -4,7 +4,7 @@ namespace didaktos.backend.Interfaces
 {
     public interface ICourseRepository
     {
-        Task<Course?> GetCourseByIdAsync(Guid courseId);
+        Task<Course?> GetCourseByIdAsync(Guid courseId); // updated with Orlando's implementation
         Task<bool> CourseExistsAsync(Guid courseId);
         Task<bool> IsUserInstructorOfCourseAsync(Guid userId, Guid courseId);
         Task<bool> IsUserEnrolledInCourseAsync(Guid userId, Guid courseId);
