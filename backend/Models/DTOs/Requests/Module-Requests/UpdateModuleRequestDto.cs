c725c0e2b67cadec714a97f05e95e65edcda2edb
Luis Barrera <124116776+Lbar23@users.noreply.github.com>
2025-09-25 22:36:06 -0500

    Updated min length for module title

--- backend/Models/DTOs/Requests/Module-Requests/UpdateModuleRequestDto.cs
+++ backend/Models/DTOs/Requests/Module-Requests/UpdateModuleRequestDto.cs
@@ -5,7 +5,7 @@ namespace didaktos.backend.Models.DTOs
     public class UpdateModuleRequestDto
     {
         [Required]
-        [StringLength(255, MinimumLength = 1)]
+        [StringLength(255, MinimumLength = 3)]
         public string Title { get; set; } = string.Empty;
     }
 }
