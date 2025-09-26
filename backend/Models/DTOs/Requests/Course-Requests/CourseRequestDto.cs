a20ab22ad42f9df77c4f448dcd5547a5f404a15a
Luis Barrera <124116776+Lbar23@users.noreply.github.com>
2025-09-25 22:27:02 -0500

    Fixed edit course validation discrepancy

--- backend/Models/DTOs/Requests/Course-Requests/CourseRequestDto.cs
+++ backend/Models/DTOs/Requests/Course-Requests/CourseRequestDto.cs
@@ -5,7 +5,7 @@ namespace didaktos.backend.Models.DTOs
     public class CourseRequestDto
     {
         [Required]
-        [MinLength(8, ErrorMessage = "The title must be at least 8 characters")]
+        [MinLength(3, ErrorMessage = "The title must be at least 3 characters")]
         public string Title { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
