f55e4276d57141f3aa765dd3e9bc9313a2dceb5c
Bryan Lazo <blazo.dev@gmail.com>
2025-09-26 13:38:09 -0500

    fix: update submission content validation to require a minimum of 10 characters

--- frontend/lib/schemas/submission.ts
+++ frontend/lib/schemas/submission.ts
@@ -3,7 +3,6 @@ import z from "zod";
 export const submissionSchema = z.object({
     content: z
         .string()
-        .min(1, { message: "Submission content is required" })
         .min(10, {
             message: "Submission content must be at least 10 characters",
         })
